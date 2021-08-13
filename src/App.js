c38e4f06a34a9dedce2c5c8e6f2f4e4078e3c39d
rcbdelarosa <rcb.delarosa@gmail.com>
2021-08-13 18:31:12 -0500

    version two of portfolio have some errors

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from "./components/navbar/index";
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+        <Router>
+            <Navbar />
+        </Router>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Nav, NavLink} from './navBar.style'
+
+const Navbar =() => {
+    return (
+        <>
+            <Nav>
+                <NavLink to='/'>NAV</NavLink>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar;
